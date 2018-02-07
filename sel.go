6d5846e55dc82f8237d228bab57659dd2845ae5b
Jbegley1995 <Jbegley1995@gmail.com>
2018-02-07 11:54:44 -0500

    update deadline for selectors.

--- sel.go
+++ sel.go
@@ -63,7 +63,7 @@ func (s *Selector) Do(ctxt context.Context, h cdp.Executor) error {
 	}
 
 	// TODO: fix this
-	ctxt, cancel := context.WithTimeout(ctxt, 100*time.Second)
+	ctxt, cancel := context.WithTimeout(ctxt, 1*time.Hour)
 	defer cancel()
 
 	var err error
